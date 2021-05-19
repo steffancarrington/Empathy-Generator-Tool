3f589219bdf9d23afc204bea11750888d99d9fab
Steffan Carrington <steffan.carrington@inviqa.com>
2021-05-19 07:55:24 +0100

    Editable sentence content component added along with Accessibility improvements to make app naviagble with keyboard.

--- src/components/GenerateSentence/index.js
+++ src/components/GenerateSentence/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './GenerateSentence.module.scss';
+
+// Editable Component
+import Editable from '../EditableSentence';
 
 const GenerateSentence = ({
   name,
@@ -10,12 +13,35 @@ const GenerateSentence = ({
   organisational,
   toggleActive
 }) => {
+  // const [editableDimension, setEditableDimension] = useState(objective);
+
+  // Create a reference using use reference and add the ref={inputRef} to input element
+  const inputRef = useRef();
+  // State for Input
+  const [task, setTask] = useState('');
+
+  const editableTest = (
+    <Editable text={task} placeholder={objective} childRef={inputRef} type="input">
+      <input
+        ref={inputRef}
+        type="text"
+        name="task"
+        className=""
+        placeholder=""
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+    </Editable>
+  );
+
   return (
-    <p className={styles.sentence} aria-live="polite" suppressHydrationWarning={true}>
-      {name}, {age} years old, would like to find {objective} whilst being {biological}
-      {toggleActive && `, ${cultural} and `}
-      {toggleActive && organisational}
-    </p>
+    <>
+      <p className={styles.sentence} aria-live="polite" suppressHydrationWarning={true}>
+        {name}, {age} years old, would like to find {editableTest} whilst being {biological}
+        {toggleActive && `, ${cultural} and `}
+        {toggleActive && organisational}
+      </p>
+    </>
   );
 };
 
