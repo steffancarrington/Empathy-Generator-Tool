2d1d93497d8497adb6081e218374e4a35f03cdce
Steffan Carrington <steffan.carrington@inviqa.com>
2021-05-28 07:22:38 +0100

    Clear Button added for editable text field on the app. Styles added to style the button.

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public'
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
   }
 });
