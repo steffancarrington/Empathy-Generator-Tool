7824be3a3ae39504f795ce6254c06eaef590cead
Steffan Carrington <45564901+steffancarrington@users.noreply.github.com>
2025-05-05 10:13:22 +0100

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,15 +3,14 @@ import 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
-  apiKey: 'AIzaSyBWZiX0wQn1NiFczE8-cDS9ONechbYV3OU',
-  authDomain: 'inclusive-scenario-generator.firebaseapp.com',
-  databaseURL:
-    'https://inclusive-scenario-generator-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'inclusive-scenario-generator',
-  storageBucket: 'inclusive-scenario-generator.appspot.com',
-  messagingSenderId: '161056727238',
-  appId: '1:161056727238:web:3cc015fb3ed08425db77c7',
-  measurementId: 'G-2XDTQYW8D3'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
 
 // Initialise Firebase App passing in our config if there are no insitialisations yet
